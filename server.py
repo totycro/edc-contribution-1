e217c8c4ff361658d1a940666db6af4e3d478592
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-04-28 09:36:19 +0200

    Make server show the available environment variables

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from flask import Flask, escape, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!\n'
+    return f"""<html>
+    <h1>EDC App Contribution Demonstration</h1>
+
+    This application has been launched with the following environment variables:
+    <ul>
+        {"".join("<li>" + key + "</li>" for key in os.environ)}
+    </ul>
+
+    </html>
+    """
 
